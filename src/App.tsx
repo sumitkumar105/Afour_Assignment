f6c01f8d650eb28fd6cecb17500b26dc71f6d967
sumitkumar105 <72189267+sumitkumar105@users.noreply.github.com>
2021-09-23 00:00:08 +0530

    first app in tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter,setCounter]=useState<number>(0);
   return (
     <div className="App">
-      <h1>hello sujitkuamr welcome to type script</h1>
-      <h2>hello sumitkumar deshpande</h2>
+      <h1>counter: {counter}</h1>
+      <button onClick={()=>setCounter(counter+1)}>clicked me</button>
     </div>
   );
 }
