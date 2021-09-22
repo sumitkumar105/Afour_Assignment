c5790b31c45256bc4d4385f931b613b3353a5de2
Sumitkumar Deshpande <72189267+sumitkumar105@users.noreply.github.com>
2021-09-22 23:25:29 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello sumitkumar welcome to type script</h1>
+      <h1>hello sujitkuamr welcome to type script</h1>
     </div>
   );
 }
