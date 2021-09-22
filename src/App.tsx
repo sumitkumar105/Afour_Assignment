b05531357f3e57d4b036b8183d96ca2c3badb7dc
sumitkumar105 <72189267+sumitkumar105@users.noreply.github.com>
2021-09-23 00:04:45 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
   const [counter,setCounter]=useState<number>(0);
   return (
     <div className="App">
+
       <h1>counter: {counter}</h1>
+      <h2>man is made by his belife if he has belife so he is</h2>
       <button onClick={()=>setCounter(counter+1)}>clicked me</button>
     </div>
   );
